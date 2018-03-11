53668a626a3095da703419db97d088d72ab9c4c6
Nica <33498296+godstopme@users.noreply.github.com>
2018-03-12 00:01:19 +0300

    json structs scaffs

---
+++ dvach/structs.go
@@ -0,0 +1,41 @@
+package dvach
+
+import (
+	"encoding/json"
+)
+
+type AttachedFile struct {
+	DisplayName   string `json:"displayname"`
+	FullName      string `json:"fullname"`
+	Name          string `json:"name"`
+	MD5           string `json:"md5`
+	Path          string `json:"path"`
+	ThumbnailPath string `json:"thumbnail"`
+	Type          int    `json:"type"`
+	NSFW          int    `json:"nsfw"`
+}
+
+type Post struct {
+	Comment   string         `json:"comment"`
+	Timestamp uint64         `json:"timestamp"`
+	Files     []AttachedFile `json:"files"`
+}
+
+type thread struct {
+	Posts []Post `json:"posts"`
+}
+
+type board struct {
+	Thread []thread `json:"threads"`
+}
+
+func UnmarshalPosts(threadData []byte) ([]Post, error) {
+	var b board
+
+	err := json.Unmarshal(threadData, &b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Thread[0].Posts, nil
+}
--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 	"sync"
 
 	"github.com/godstopme/myonmyon/fetch"
-	"github.com/godstopme/myonmyon/utils"
+    "github.com/godstopme/myonmyon/utils"
+    "github.com/godstopme/myonmyon/dvach"
 )
 
 const (
@@ -37,13 +38,10 @@ func main() {
 
 	worker := fetch.NewFetchingWorker(1)
 
-	worker.Urls <- "https://2ch.hk/b/res/172168647.json"
-	worker.Urls <- "https://2ch.hk/b/threads.json"
-
 	wg.Add(2)
 
 	go worker.Work(true)
-
+    worker.Urls <- "https://2ch.hk/b/res/172253674.json"
 	go func(w *fetch.FetchingWorker) {
 		defer wg.Done()
 
@@ -52,7 +50,13 @@ func main() {
 			log.Printf("Got result from %s\n", result.URL)
 
 			i++
-			utils.Dump(fmt.Sprintf("dump%v.json", i), string(result.Result))
+            utils.Dump(fmt.Sprintf("dump%v.json", i), string(result.Result))
+            posts, err := dvach.UnmarshalPosts(result.Result)
+            if err != nil {
+                log.Println("Error unmarshaling json!", err)
+            } else {
+                log.Println(posts)
+            }
 		}
 		log.Println("Processed all results")
 	}(worker)
